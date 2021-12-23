e6729eafe49c91c793b84eaecec08ac14748f1cf
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-12-23 12:40:12 -0800

    set up chart settings

---
+++ settings/ChartSettings.js
@@ -0,0 +1,40 @@
+let chartSettings = {
+    width: 600,
+    height: 300,
+    layout: {
+        backgroundColor: '#ffffff',
+        textColor: 'rgba(33, 56, 77, 1)',
+    },
+	grid: {
+        horzLines: {
+          color: '#F0F3FA',
+        },
+        vertLines: {
+          color: '#F0F3FA',
+        },
+    },
+    /* comment out because LightweightCharts is not loaded
+    crosshair: {
+		mode: LightweightCharts.CrosshairMode.Normal,
+	},*/
+    crosshair: { mode: 0},
+	rightPriceScale: {
+		borderColor: 'rgba(197, 203, 206, 0.8)',
+	},
+	timeScale: {
+		borderColor: 'rgba(197, 203, 206, 0.8)',
+	},
+};
+
+let candlestickSeriesSettings = {
+    upColor: 'rgb(38,166,154)',
+    downColor: 'rgb(255,82,82)',
+    wickUpColor: 'rgb(38,166,154)',
+    wickDownColor: 'rgb(255,82,82)',
+    borderVisible: false,
+};
+
+window.TradingApp_Settings_Tradingview = {
+    'chartSettings': chartSettings,
+    'candlestickSeriesSettings': candlestickSeriesSettings
+};
