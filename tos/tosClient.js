1b71338dab7daa05c8c36f020229797e98c40ef0
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-12-24 00:32:14 -0800

    test place order working

--- tos/tosClient.js
+++ tos/tosClient.js
@@ -1,27 +1,14 @@
-window.TradingApp_TOS = (function() {
+window.TradingApp.TOS = (function () {
     const getPriceHistory = (text) => {
         console.log('get price history');
         let url = "https://api.tdameritrade.com/v1/marketdata/SPY/pricehistory?apikey={client_id}&frequencyType=minute&frequency=1&startDate=1640010600000&endDate=1640297198351";
-        return fetch(url).then(function(response) {
-            return response.json();
-        }).then(function(data) {
-            let candles = [];
-            data.candles.forEach(element => {
-                const d = new Date(element.datetime);
-                const newD = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()) / 1000;
-                candles.push({
-                    time: newD, 
-                    open: element.open, 
-                    high: element.high, 
-                    low: element.low, 
-                    close: element.close,
-                    volume: element.volume
-                });                  
-            });
-            return candles;
-        }).catch(function() {
-            console.log("Booo");
-        });
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, false); // false for synchronous request
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+
+
     };
 
     const getSamplePriceHistory = () => {
@@ -31,19 +18,41 @@ window.TradingApp_TOS = (function() {
             const d = new Date(element.datetime);
             const newD = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()) / 1000;
             candles.push({
-                time: newD, 
-                open: element.open, 
-                high: element.high, 
-                low: element.low, 
+                time: newD,
+                open: element.open,
+                high: element.high,
+                low: element.low,
                 close: element.close,
                 volume: element.volume
-            });  
+            });
         });
         return candles;
     };
-  
+
+    const placeOrderBase = (order) => {
+        let accountId = window.TradingApp.Secrets.accountId;
+        let url = `https://api.tdameritrade.com/v1/accounts/${accountId}/orders`;
+        console.log(url);
+        const config = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + window.TradingApp.Secrets.accessToken
+            },
+            body: JSON.stringify(order)
+        }
+        fetch(url, config)
+    };
+
+    const testOrder = () => {
+        order = { "orderId": null, "quantity": 0.0, "orderLegCollection": [{ "orderLegType": "EQUITY", "instrument": { "assetType": "EQUITY", "symbol": "MSFT" }, "instruction": "BUY", "quantity": 1.0 }], "session": "NORMAL", "duration": "DAY", "orderType": "MARKET", "orderStrategyType": "SINGLE", "childOrderStrategies": null, "price": null, "stopPrice": null, "status": null };
+        placeOrderBase(order);
+    };
+
     return {
         getPriceHistory,
-        getSamplePriceHistory
+        getSamplePriceHistory,
+        testOrder
     }
 })();
