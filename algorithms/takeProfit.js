7f4a0bc28916d7985638f5e1dd9fc19e0ab58049
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-12-26 09:05:18 -0800

    setup breakout algorithm

---
+++ algorithms/takeProfit.js
@@ -0,0 +1,8 @@
+window.TradingApp.Algo.TakeProfit = (function () {
+    const getProfitTargets = (totalShares, entryPrice, stopOut, setupQuality) => {
+        return [];
+    };
+    return {
+        getProfitTargets
+    };
+})();
