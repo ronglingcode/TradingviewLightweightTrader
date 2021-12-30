1710366fbe22f0d695fae99a047644f63b63ca2e
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-12-29 19:23:11 -0800

    bump risk by 10% to 161

--- algorithms/riskManager.js
+++ algorithms/riskManager.js
@@ -1,5 +1,5 @@
 window.TradingApp.Algo.RiskManager = (function () {
-    const DefaultMaxRiskPerTrade = 146.41;
+    const DefaultMaxRiskPerTrade = 161;
     const MaxCapitalPerTrade = 100000;
     const getMaxRiskPerTrade = (setupQuality, multiplier) => {
         return multiplier * DefaultMaxRiskPerTrade;
