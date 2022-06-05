04a7d04641888cd2cbcaa1739ef3120516d3ee44
rongvrv <77297902+rongvrv@users.noreply.github.com>
2022-06-05 08:02:10 -0700

    calculate remaining risk

--- algorithms/riskManager.js
+++ algorithms/riskManager.js
@@ -3,7 +3,17 @@ window.TradingApp.Algo.RiskManager = (function () {
     const MaxDailyLoss = 2.5 * DefaultMaxRiskPerTrade;
     const MaxCapitalPerTrade = 100000;
     const getMaxRiskPerTrade = (setupQuality, multiplier) => {
-        return multiplier * DefaultMaxRiskPerTrade;
+        let pnl = window.TradingApp.Firestore.getProfitAndLossFromCache();
+        let defaultRisk = multiplier * DefaultMaxRiskPerTrade;
+        // currently negative profit loss on the day
+        if (pnl && pnl < 0) {
+            let remainingRisk = MaxDailyLoss + pnl;
+            // 1R, 0.75R, 0.325R
+            return Math.min(remainingRisk / 2, defaultRisk);
+        } else {
+            return defaultRisk;
+        }
+
     };
     const addCents = (price, cents) => {
         price = Math.ceil(price * 100);
