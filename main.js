a7d159cfb6cb9cb4a664b4262e14d86000726d98
rongvrv <77297902+rongvrv@users.noreply.github.com>
2022-06-11 17:16:49 -0700

    use all 10 number keys for adjusting exit orders

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ htmlBody.addEventListener("keydown", async function (keyboardEvent) {
     } else if (code === "KeyF") {
         window.TradingApp.TOS.flattenPosition(symbol);
         window.TradingApp.Firestore.logInfo("flatten for " + symbol);
-    } else if (["Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6"].includes(code)) {
+    } else if (["Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0"].includes(code)) {
         window.TradingApp.TOS.adjustOrder(symbol, code);
     } else if (code === 'KeyT') {
         // move stop orders
