062b538499bdc40ee4a1422bd457dac4632395f1
rongvrv <77297902+rongvrv@users.noreply.github.com>
2022-06-17 11:03:51 -0700

    add watch list

--- data.js
+++ data.js
@@ -1,5 +1,10 @@
 window.TradingData = {
     'StockSelection': {
         '6/10/2022': ['DOCU', 'NFLX', 'TSLA', 'SPY'],
+        '6/13/2022': ['AMD', 'TSLA', 'QQQ', 'SPY'],
+        '6/14/2022': ['ORCL', 'COIN', 'QQQ', 'SPY'],
+        '6/15/2022': ['QQQ', 'SPY', 'SNOW', 'NIO'],
+        '6/16/2022': ['QQQ', 'SPY', 'TSLA', 'BA'],
+        '6/17/2022': ['PDD', 'SPY', 'QQQ', 'ROKU'],
     }
 };
